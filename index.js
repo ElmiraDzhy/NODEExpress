5fdb186a09f8082286ed079fc56804aaf8cd47d2
ElyaDzhy <elmira.freshcode@gmail.com>
2023-05-19 12:29:41 +0300

    Refactor

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 const bodyParser = express.json();
-const { validateMedicine } = require("./middleware/validateMedicine");
+const { validateMedicine, validateUpdMed } = require("./middleware/validateMedicine");
 const MedicineController = require("./controllers/Medicine.controller");
 
+//POST
 app.post("/medicine", bodyParser, validateMedicine, MedicineController.addMedicine);
+
+//GET
 app.get("/medicine", MedicineController.getAllMed);
 app.get("/medicine/:name", MedicineController.getOneMed);
+
+//PATCH
 app.patch("/medicine", bodyParser, MedicineController.updateMed);
+
+//DELETE
 app.delete("/medicine/:name", MedicineController.deleteMed);
 
 app.listen(3000);
